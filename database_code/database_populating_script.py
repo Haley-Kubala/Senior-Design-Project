7f49131b0cb76df1f5fa7c9f5ba59ffa3d5eb3fd
Haley Kubala <Kubala.Haley17@libertycommon.org>
2017-02-08 13:36:34 -0700

    added argparse and mongo database connection

--- database_code/database_populating_script.py
+++ database_code/database_populating_script.py
@@ -1,7 +1,20 @@
+import argparse
 import pymongo
 from pymongo import MongoClient
 
+#specifiy port later
 client = MongoClient()
+db = client.test_database
+collection = db.test_collection
+
+studentDict = {}
+
+def fill_dict():
+    
+
+parser = argparse.ArugmentParser()
+parser.add_argument("file", help="enter path to a text file")
+args = parser.parse_args()
 
 #(1)establish connection to database
 #(2)create dictionary for each individual student in the for loop
