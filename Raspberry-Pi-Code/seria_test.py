a8e73e0cc6e66c97eacceb061bc3d9b1cf9024c7
David Rohrbaugh <rohrbaugh.david17@libertycommon.org>
2017-02-22 15:18:18 -0700

    Few more modifications

--- Raspberry-Pi-Code/seria_test.py
+++ Raspberry-Pi-Code/seria_test.py
@@ -1,12 +1,15 @@
 import serial
 
-ser = serial.Serial('/dev/ttyACM0',9600)
+serial = serial.Serial('/dev/ttyACM0',9600)
 s = [0,1]
+
+time.sleep(100)
+
 while True:
-	read_serial=ser.readline()
-	s[0] = str(int(ser.readline(),16))
+	read_serial=serial.readline()
+	s[0] = str(int(serial.readline(),16))
 	print s[0]
 	print read_serial
 	time.sleep(2)
-	ser.write(s[0])
+	serial.write(s[0])
 	time.sleep(0.05)
