1a92b48a2cf58ad0f1f2e1dc95cb8464190f36ad
Haley Kubala <Kubala.Haley17@libertycommon.org>
2017-02-27 13:56:30 -0700

    testing again

--- store_data/store_data_LEAVE.py
+++ store_data/store_data_LEAVE.py
@@ -5,7 +5,7 @@ student returns.
 '''
 import argparse
 
-def main(args):
+def main(args["START_TIME"]):
     leave_info_file = open('leave_info_file', 'w')
     leave_info_file.write(args["ID"], args["START_TIME"], args["END_TIME"])
 
