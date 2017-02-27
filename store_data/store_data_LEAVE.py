310e84d28d6be58e59a0b9973ee5643680cc205e
Haley Kubala <Kubala.Haley17@libertycommon.org>
2017-02-27 13:52:11 -0700

    added main function

--- store_data/store_data_LEAVE.py
+++ store_data/store_data_LEAVE.py
@@ -5,13 +5,15 @@ student returns.
 '''
 import argparse
 
-leave_info_file = open('leave_info_file', 'w')
+def main():
+    leave_info_file = open('leave_info_file', 'w')
+    leave_info_file.write(args["ID"], args["START_TIME"], args["END_TIME"])
 
-leave_info_file.write(args["ID"], args["START_TIME"], args["END_TIME"])
 
-
-parser = argparse.ArgumentParser()
-parser.add_argument("ID", help="student ID needs to be entered")
-parser.add_argument("START_TIME", help="beginning time stamp")
-parser.add_argument("END_TIME", help="ending time stamp")
-args = vars(parser.parse_args())
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("ID", help="student ID needs to be entered")
+    parser.add_argument("START_TIME", help="beginning time stamp")
+    parser.add_argument("END_TIME", help="ending time stamp")
+    args = vars(parser.parse_args())
+    main(args)
