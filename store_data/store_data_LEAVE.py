4a322af61f3b288673b07ed2cbbb0d189132515a
Haley Kubala <Kubala.Haley17@libertycommon.org>
2017-02-28 13:45:26 -0700

    added datetime function

--- store_data/store_data_LEAVE.py
+++ store_data/store_data_LEAVE.py
@@ -4,10 +4,18 @@ and store it in a text file so we can access it later when the
 student returns.
 '''
 import argparse
+import datetime
+
 
 def main(args):
     leave_info_file = open('leave_info_file', 'w')
     leave_info_file.write("ID : %s Time out : %s" % (args["ID"], args["START_TIME"]))
+
+
+def create_date(args):
+    date_time = datetime.datetime.now().time()
+    return date_time
+    #date_time.strftime()
 
 '''maybe create a function that creates a dat time and formats it as
     a string. put it in utils library and it can be called on the return script
@@ -16,6 +24,8 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("ID", help="student ID needs to be entered")
-    parser.add_argument("START_TIME", help="beginning time stamp")
+    #parser.add_argument("START_TIME", help="beginning time stamp")
     args = vars(parser.parse_args())
     main(args)
+
+print create_date(args)
