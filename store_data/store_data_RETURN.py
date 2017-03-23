104a99da6302be66531fc812d2b7ccd449a4a006
Haley Kubala <Kubala.Haley17@libertycommon.org>
2017-03-23 13:53:44 -0600

    debugging once again

--- store_data/store_data_RETURN.py
+++ store_data/store_data_RETURN.py
@@ -9,9 +9,9 @@ from datetime import date
 import time
 
 #def main(args):
-    end_date_time = datetime.datetime.now()
-    with open('leave_info_file') as leave_info_file:
-        ID_start_date_time = leave_info_file.read()
-        print ID_start_date_time
+end_date_time = datetime.datetime.now()
+with open('leave_info_file') as leave_info_file:
+    ID_start_date_time = leave_info_file.read()
+    print ID_start_date_time
     #open leave_info_file
     #read ID and start_date_time then store them as variables
