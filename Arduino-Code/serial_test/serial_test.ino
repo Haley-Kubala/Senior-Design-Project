bdee3306fd29a589a09837d0e3702c5f2198bc51
drohrbaugh9 <Rohrbaugh.David17@libertycommon.org>
2017-03-23 19:58:03 +0000

    Receive and send back confirmation signal

--- Arduino-Code/serial_test/serial_test.ino
+++ Arduino-Code/serial_test/serial_test.ino
@@ -1,31 +1,31 @@
 char sending[50] = "Sending data...";
 char test[50] = "77778";
 char test2[50] = "77777";
-char input[50];
+int input;
 //int a = 0; 
 
 void setup() {
-  Serial.begin(9600);              //Starting serial communication
+  Serial.begin(9600);
   pinMode(13, OUTPUT);
   digitalWrite(13, LOW);
   
-  delay(1000);
+  delay(100);
   
   Serial.println(sending);
   
-  delay(1000);
+  delay(100);
   
   Serial.println(test);
   
-  delay(1000);
+  delay(100);
   
   Serial.println(sending);
   
-  delay(1000);
+  delay(100);
   
-  Serial.println(test2);
+  Serial.printlnchar(test2);
   
-  delay(1000);
+  delay(100);
 }
   
 void loop() {
@@ -33,13 +33,13 @@ void loop() {
     input = Serial.read();
     Serial.println(input);
     
-    if (input.equals("48")) { // 48 is ASCII for 0 (zero)
+    if (input == 48) { // 48 is ASCII for 0 (zero)
       digitalWrite(13, LOW);
     }
     
-    if (input.equals("49")) { // 49 is ASCII for 1 (one)
+    if (input == 49) { // 49 is ASCII for 1 (one)
       digitalWrite(13, HIGH);
     }      
   }
-  delay(1000);
+  delay(100);
 }
