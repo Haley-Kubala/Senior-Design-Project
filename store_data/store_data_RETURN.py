16d1bb46c6b66ae8036900d2a44510da5970e0b3
Haley Kubala <Kubala.Haley17@libertycommon.org>
2017-03-29 13:09:12 -0600

    debugging .split()

--- store_data/store_data_RETURN.py
+++ store_data/store_data_RETURN.py
@@ -13,7 +13,7 @@ end_date_time = datetime.datetime.now()
 with open('leave_info_file') as leave_info_file:
     ID_leave_time_stamp = leave_info_file.read()
     print type(ID_leave_time_stamp)
-    info_list = ID_leave_time_stamp.split(" ", 1)
+    info_list = ID_leave_time_stamp.split(" ", 0)
     print info_list
     ID = info_list[1]
     date = info_list[2]
