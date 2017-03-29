12973511985bbc2eef881d5cc19d27de1f35acbe
Haley Kubala <Kubala.Haley17@libertycommon.org>
2017-03-29 14:10:23 -0600

    got variables all correctly working, now it's time to connect to mongo and make a document. yay

--- store_data/store_data_RETURN.py
+++ store_data/store_data_RETURN.py
@@ -8,12 +8,11 @@ from datetime import date
 import time
 
 end_date_time = datetime.datetime.now()
-print end_date_time
+
 with open('leave_info_file') as leave_info_file:
     ID_leave_time_stamp = leave_info_file.read()
     print type(ID_leave_time_stamp)
     info_list = ID_leave_time_stamp.split(" ")
-    print info_list
     ID = info_list[1]
     date = info_list[2]
     time = info_list[3]
