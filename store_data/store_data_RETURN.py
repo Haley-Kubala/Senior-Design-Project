a1ef78be2a3fb8b0f0211e5883c74aac83e20f1d
Haley Kubala <Kubala.Haley17@libertycommon.org>
2017-03-29 15:10:05 -0600

    cleaned up RETURN script and got rid of unneccessary print statements. At this point part of the script is fuctional. Need to add mongo connection and document creation/database manipulation

--- store_data/store_data_RETURN.py
+++ store_data/store_data_RETURN.py
@@ -11,7 +11,6 @@ end_date_time = datetime.datetime.now()
 
 with open('leave_info_file') as leave_info_file:
     ID_leave_time_stamp = leave_info_file.read()
-    print type(ID_leave_time_stamp)
     info_list = ID_leave_time_stamp.split(" ")
     ID = info_list[1]
     date = info_list[2]
