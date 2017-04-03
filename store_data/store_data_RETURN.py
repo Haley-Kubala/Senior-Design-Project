72f0ee0dce5c073d3ffd5b019e10bfbfd24f1607
Haley Kubala <Kubala.Haley17@libertycommon.org>
2017-04-03 13:39:49 -0600

    RETURN script now needs to read in values from annie's script

--- store_data/store_data_RETURN.py
+++ store_data/store_data_RETURN.py
@@ -1,17 +1,33 @@
 #connect to mongo database
 #connect to mongo collection
 #create new document in collection
-
+#adjust script so it takes ID and Timestamp as args
 import argparse
 import datetime
 from datetime import date
 import time
+import collections
+import mongodb
+import pymongo
+from pymongo import MongoClient
 
-end_date_time = datetime.datetime.now()
 
-with open('leave_info_file') as leave_info_file:
+def main(args):
+    end_date_time = datetime.datetime.now()
+    print args
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("ID", help="student ID needs to be entered")
+    parser.add_argument("beginning_timestamp", help="")
+    args = vars(parser.parse_args())
+    main(args)
+
+
+'''with open('leave_info_file') as leave_info_file:
     ID_leave_time_stamp = leave_info_file.read()
     info_list = ID_leave_time_stamp.split(" ")
     ID = info_list[1]
     date = info_list[2]
     time = info_list[3]
+'''
