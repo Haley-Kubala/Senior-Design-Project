5815b397b30d70ee9d9c53bfaa0dcb06996f27d4
Haley Kubala <Kubala.Haley17@libertycommon.org>
2017-04-24 13:48:05 -0600

    removed db.... from command

--- store_data/store_data_RETURN.py
+++ store_data/store_data_RETURN.py
@@ -25,7 +25,7 @@ def store_data(collection_name, id_args, leave_time, return_time):
     '''Takes input from annie's script and creates
     new mongo document in correct collection.
     '''
-    db.collection_name.insert({"id": id_args,
+    collection_name.insert({"id": id_args,
                                 "leave_time" : leave_time,
                                 "return_time" : return_time})
 
