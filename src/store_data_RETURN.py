6b0c6ac8f10adac1d195e31d40767e06ef7d4c03
Haley Kubala <Kubala.Haley17@libertycommon.org>
2017-05-15 13:40:32 -0600

    that moment when you don't even call the function by the right name :/

--- src/store_data_RETURN.py
+++ src/store_data_RETURN.py
@@ -45,7 +45,7 @@ def store_data(collection_name, id_args, leave_time, return_time):
 
 
 def write_to_csv(collection, ID):
-    queries = find_one(collection, ID);
+    queries = find_id(collection, ID);
     with open('mongo_queries.csv', 'w') as csv_file:
         field_names = ["id", "student_info"]
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
