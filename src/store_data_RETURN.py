5a04381d4658252d9e56014c5dd875e95841198f
Haley Kubala <Kubala.Haley17@libertycommon.org>
2017-05-15 13:55:16 -0600

    fieldnames is being weird

--- src/store_data_RETURN.py
+++ src/store_data_RETURN.py
@@ -48,7 +48,7 @@ def write_to_csv(collection, ID):
     queries = find_id(collection, ID);
     print queries
     with open('mongo_queries.csv', 'w') as csv_file:
-        field_names = ["id"]
+        field_names = ["u"]
         writer = csv.DictWriter(csv_file, fieldnames=field_names)
         for document in queries:
             writer.writerow(document)
